57e071791f94ef0e24fea2db4bfd6b0250dc914a
jmg <jmg@nve.no>
2019-07-09 20:52:33 +0200

    update to new syntax

--- deps/build.jl
+++ deps/build.jl
@@ -1,8 +1,10 @@
+using Libdl
+
 mods = ["MODULES.f90"];
 
 routines = ["CUMULATE.f90", "DRIVE.f90", "FSM.f90", "INITIALIZE.f90", "OUTPUT.f90", "PHYSICS.f90", "QSAT.f90", "SET_PARAMETERS.f90", "SNOW.f90", "SOIL.f90", "SURF_EBAL.f90", "SURF_EXCH.f90", "SURF_PROPS.f90", "TRIDIAG.f90"];
 
-if is_windows()
+if Sys.iswindows()
     flags = ["-m$(Sys.WORD_SIZE)", "-shared", "-O3"]
 else
     flags = ["-m$(Sys.WORD_SIZE)", "-shared", "-O3", "-fPIC"]
